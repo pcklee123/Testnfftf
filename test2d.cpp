4707ee0a693736e3284af6e0f3f17d0f5ccfe55f
pcklee123 <pcklee123@gmail.com>
2023-04-20 18:02:36 +0800

    a

--- test2d.cpp
+++ test2d.cpp
@@ -19,63 +19,51 @@ int main(void)
     int N1 = 12; // x
     int N2 = 14; // y
     int N3 = 16; // z
-    int maxcells = 32;
-    int ncomponents = 3;
-    int n_space_div[] = {N3, N2, N1};
-    float dd[] = {1.0f / (float)N1, 1.0f / (float)N2, 1.0f / (float)N3};
-    float posl[] = {-0.5, -0.5, -0.5};
+//    int maxcells = 32;
+//    int ncomponents = 3;
+    //  int n_space_div[] = {N3, N2, N1};
+    double dd[] = {1.0, 1.0, 1.0};
+    double posl[] = {-(double)N1 / 2, -(double)N2 / 2, -(double)N3 / 2};
     int N[] = {N1, N2, N3};
     int M = N1 * N2 * N3;
     nfftf_plan p;
-    int xi = (n_space_div[0] - 1) / maxcells + 1;
-    int yj = (n_space_div[1] - 1) / maxcells + 1;
-    int zk = (n_space_div[2] - 1) / maxcells + 1;
-    int nx = n_space_div[0] / xi;
-    int ny = n_space_div[1] / yj;
-    int nz = n_space_div[2] / zk;
+
     nfftf_init(&p, 3, N, M);
-    // nfftf_init_2d(&p, N1, N2, M);
     for (int k = 0; k < N3; ++k)
         for (int j = 0; j < N2; ++j)
-            for (int i = 0; i < N1; ++i)
+            for (int i = 0; i < N1; ++i) // i is x-coord and array is [k][j][i]
             {
                 int n = (k * N2 + j) * N1 + i;
-                //int n = (i * N2 + j) * N1 + k;
-                p.x[3 * n] = -0.5 + (float)i / N1; //p.x[n][0]=x ..,y,z
-                int n = (i * N2 + j) * N1 + k;
-                p.x[3 * n] = -0.5 + (float)i / N1;
+                // int n = (i * N2 + j) * N1 + k;
+                p.x[3 * n] = -0.5 + (float)i / N1; // p.x[n][0]=x ..,y,z
                 p.x[3 * n + 1] = -0.5 + (float)j / N2;
                 p.x[3 * n + 2] = -0.5 + (float)k / N3;
                 if (i == N1 / 2)
-                    p.f[n][0] = 1.0; /* Memory corruption? */
+                    p.f[n][0] = 1.0;
                 else
                     p.f[n][0] = 0.0;
                 p.f[n][1] = 0.0;
             }
 
     vtkSmartPointer<vtkImageData> imageData = vtkSmartPointer<vtkImageData>::New(); // Create the vtkImageData object
-    imageData->SetDimensions(n_space_div);                                           // Set the dimensions of the image data
-    imageData->SetSpacing(dd[0] * xi, dd[1] * yj, dd[2] * zk);
-    imageData->SetOrigin(posl[0], posl[1], posl[2]); // Set the origin of the image data
-    imageData->AllocateScalars(VTK_FLOAT, ncomponents);
+    imageData->SetDimensions(N);                                                    // Set the dimensions of the image data
+    imageData->SetSpacing(dd[0], dd[1], dd[2]);                                     // x,y,z
+    imageData->SetOrigin(posl[0], posl[1], posl[2]);                                // Set the origin of the image data
+    imageData->AllocateScalars(VTK_FLOAT, 3);
     imageData->GetPointData()->GetScalars()->SetName("f");
     float *data2 = static_cast<float *>(imageData->GetScalarPointer()); // Get a pointer to the density field array
 
-    for (int k = 0; k < nz; ++k)
-    {
-        for (int j = 0; j < ny; ++j)
-        {
-            for (int i = 0; i < nx; ++i)
+    for (int k = 0; k < N3; ++k)
+        for (int j = 0; j < N2; ++j)
+            for (int i = 0; i < N1; ++i) // i is x-coord and array is [k][j][i]
             {
-
-                int n = (k * ny + j) * nx + i;
-                int n1 = (i * ny + j) * nx + k;
+                int n = (k * N2 + j) * N1 + i;
+                // int n = (i * N2 + j) * N1 + k;
+                //                int n1 = (i * ny + j) * nx + k;
                 data2[3 * n + 2] = 0;
                 for (int c = 0; c < 2; ++c)
                     data2[3 * n + c] = p.f[n][c];
             }
-        }
-    }
 
     vtkSmartPointer<vtkXMLImageDataWriter> writer = vtkSmartPointer<vtkXMLImageDataWriter>::New(); // Create the vtkXMLImageDataWriter object
     writer->SetFileName("f.vti");                                                                  // Set the output file name                                                                     // Set the time value
@@ -87,20 +75,15 @@ int main(void)
 
     imageData->GetPointData()->GetScalars()->SetName("x");
 
-    for (int k = 0; k < nz; ++k)
-    {
-        for (int j = 0; j < ny; ++j)
-        {
-            for (int i = 0; i < nx; ++i)
+    for (int k = 0; k < N3; ++k)
+        for (int j = 0; j < N2; ++j)
+            for (int i = 0; i < N1; ++i) // i is x-coord and array is [k][j][i]
             {
-                int n = (k * ny + j) * nx + i;
-                int n1 = (i * ny + j) * nx + k;
+                int n = (k * N2 + j) * N1 + i;
+                // int n = (i * N2 + j) * N1 + k;
                 for (int c = 0; c < 3; ++c)
-                    //                data2[n] = p.f_hat[n][0];
-                    data2[3 * n + c] = p.x[3*n + c];
+                    data2[3 * n + c] = p.x[3 * n + c]; // x,y,z
             }
-        }
-    }
 
     writer->SetFileName("x.vti"); // Set the output file name                                                                     // Set the time value
     writer->Write();              // Write the output file
@@ -116,22 +99,20 @@ int main(void)
     }
 
     nfftf_adjoint(&p); /* Segfault if M <= 6 */
-    nfftf_vpr_complex(p.f_hat, p.N_total, "adjoint nfft, vector f_hat");
-    nfftf_finalize(&p); /* Segfault if M > 6 */
 
     imageData->GetPointData()->GetScalars()->SetName("f_hat");
     //  float *data2 = static_cast<float *>(imageData->GetScalarPointer()); // Get a pointer to the density field array
 
-    for (int k = 0; k < nz; ++k)
+    for (int k = 0; k < N3; ++k)
     {
-        for (int j = 0; j < ny; ++j)
+        for (int j = 0; j < N2; ++j)
         {
-            for (int i = 0; i < nx; ++i)
+            for (int i = 0; i < N1; ++i)
             {
-                int n = (k * ny + j) * nx + i;
+                int n = (k * N2 + j) * N1 + i;
                 data2[3 * n + 2] = 0;
                 for (int c = 0; c < 2; ++c)
-                    data2[3 * n + c] = p.f_hat[n][0];
+                    data2[3 * n + c] = p.f_hat[n][c];
             }
         }
     }
@@ -141,5 +122,7 @@ int main(void)
     //  writer->SetInputData(imageData);   // Set the input image data
     writer->Write(); // Write the output file
 
+    nfftf_vpr_complex(p.f_hat, p.N_total, "adjoint nfft, vector f_hat");
+    nfftf_finalize(&p); /* Segfault if M > 6 */
     return 0;
 }
